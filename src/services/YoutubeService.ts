43c4c1b3e1695ddd9ac770f2caa2ef4578eaebd2
ahmniab <ahmniab11@gmail.com>
2026-02-02 18:48:46 +0200

    Refactor: YouTube API key handling to use EnvVars constants

--- src/services/YoutubeService.ts
+++ src/services/YoutubeService.ts
@@ -1,12 +1,12 @@
 import { youtube, youtube_v3 } from "@googleapis/youtube";
+import EnvVars from '../common/constants/env';
 
-const youtubeClient = process.env.YOUTUBE_API_KEY ? youtube({
+const youtubeClient = EnvVars.YoutubeApiKey ? youtube({
         version: 'v3',
-        auth: process.env.YOUTUBE_API_KEY,
+        auth: EnvVars.YoutubeApiKey,
     }) : null;
 
 export const searchYouTubeVideos = async (query: string): Promise<youtube_v3.Schema$SearchResult[]> => {
-    if (!process.env.YOUTUBE_API_KEY) return [];
     if (!youtubeClient) return [];
 
     const response = await youtubeClient.search.list({

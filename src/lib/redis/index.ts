29b734989f98e0b903b9c100c80428f91a09990a
ahmniab <ahmniab11@gmail.com>
2026-02-15 23:17:41 +0200

    Refactor Redis room management functions to use RedisRoom type and enhance user handling

--- src/lib/redis/index.ts
+++ src/lib/redis/index.ts
@@ -1,15 +1,36 @@
 import redis from './config';
-import { Room } from '../../models/types';
+import { RedisRoom, Users } from '../../models/types';
 import { jsonStringify, jsonParse } from '@src/common/utils/hepers';
 
-export const getRoom = async (roomId: string): Promise<Room | null> => {
+export const getRoom = async (roomId: string): Promise<RedisRoom | null> => {
     const room = await redis?.get(roomId);
-    if (room) return jsonParse<Room>(room);
+    if (room) return jsonParse<RedisRoom>(room);
     return null;
 }
 
-export const setRoom = async (room: Room): Promise<boolean> => {
+export const setRoom = async (room: RedisRoom): Promise<boolean> => {
     const result = await redis?.set(room.roomId, jsonStringify(room));
     return result === 'OK';
 }
+export const setTimeOut = async (roomId: string, expirationTime: number): Promise<boolean> => {
+    const result = await redis?.expire(roomId, expirationTime);
+    return result === 1;
+}
 
+export const deleteRoom = async (roomId: string): Promise<boolean> => {
+    const result = await redis?.del(roomId);
+    return result === 1;
+}
+
+export const setRoomUsers = async (roomId: string, users: Users): Promise<boolean> => {
+    const room = await getRoom(roomId);
+    if (!room) return false;
+    room.users = users;
+    return setRoom(room);
+}
+
+export const getRoomUsers = async (roomId: string): Promise<Users | null> => {
+    const room = await getRoom(roomId);
+    if (!room) return null;
+    return room.users;
+}
